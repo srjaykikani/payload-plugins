10d05269d6e24b126fb37e791587f4a2168672e1
Jens Becker <info@jhb.software>
2025-03-04 15:09:29 +0100

    fix(cloudinary): improve error handling of file deletion

--- cloudinary/src/hooks/afterDelete.ts
+++ cloudinary/src/hooks/afterDelete.ts
@@ -1,12 +1,31 @@
-import { CollectionAfterDeleteHook } from 'payload'
+import { APIError, CollectionAfterDeleteHook } from 'payload'
 import { v2 as cloudinary } from 'cloudinary'
 
 /** Hooks which deletes the file from Cloudinary */
 const afterDeleteHook: CollectionAfterDeleteHook = async ({ doc }) => {
-  await cloudinary.uploader.destroy(doc.cloudinaryPublicId, (error: any, result: any) => {
-    console.error('Error deleting file from Cloudinary:', error, result)
-  })
-  return doc
+  type ReturnType = {
+    result?: 'ok' | 'not-found' | any
+  }
+
+  const result = (await cloudinary.uploader.destroy(doc.cloudinaryPublicId)) as ReturnType
+
+  if (result?.result === 'ok') {
+    return doc
+  } else if (result?.result === 'not found') {
+    throw new APIError(
+      'File to delete not found in Cloudinary', // message
+      500, // status
+      result, // data
+      true, // isPublic
+    )
+  } else {
+    throw new APIError(
+      'Error deleting file from Cloudinary', // message
+      500, // status
+      result, // data
+      true, // isPublic
+    )
+  }
 }
 
 export default afterDeleteHook
