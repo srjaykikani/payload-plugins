f6a41df1ad73f60204d680c0ab56c11edcc82418
Jens Becker <info@jhb.software>
2025-06-22 12:59:51 +0200

    fix(pages): correct field hooks to use the correct field value

--- pages/src/hooks/beforeDuplicate.ts
+++ pages/src/hooks/beforeDuplicate.ts
@@ -1,11 +1,11 @@
 import { FieldHook } from 'payload'
 
 /** Hooks which adjusts the slug to make sure the slug is still unique after duplication. */
-export const beforeDuplicateSlug: FieldHook<any, any, any> = ({ value }) => {
-  return value?.slug ? value.slug + '-' + 'copy' : undefined
+export const beforeDuplicateSlug: FieldHook = ({ value }) => {
+  return value && typeof value === 'string' ? value + '-copy' : value
 }
 
 /** Hooks which adjusts the title to indicate this is a copy. */
-export const beforeDuplicateTitle: FieldHook<any, any, any> = ({ value }) => {
-  return value?.title ? value.title + ' - ' + 'copy' : undefined
+export const beforeDuplicateTitle: FieldHook = ({ value }) => {
+  return value && typeof value === 'string' ? value + ' (copy)' : value
 }
