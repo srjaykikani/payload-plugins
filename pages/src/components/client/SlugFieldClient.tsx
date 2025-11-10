82aecdb5c48a8347126561f4cade459753d70429
S R Jay <srjaykikani@gmail.com>
2025-11-10 05:37:16 +0530

    refactor: update SlugFieldClient to use centralized RefreshIcon
    Extract RefreshIcon to dedicated icons folder and import it.
    InfoIcon remains as a local component since no equivalent
    exists in Payload UI's icons directory.

--- pages/src/components/client/SlugFieldClient.tsx
+++ pages/src/components/client/SlugFieldClient.tsx
@@ -12,6 +12,7 @@ import type { TextFieldClientProps } from 'payload'
 import { useEffect, useState } from 'react'
 import { formatSlug, liveFormatSlug } from '../../hooks/validateSlug.js'
 import { usePluginTranslation } from '../../utils/usePluginTranslations.js'
+import { RefreshIcon } from '../../icons/RefreshIcon.js'
 
 export type SlugFieldProps = {
   pageSlug: boolean | undefined
@@ -166,8 +167,7 @@ export const SlugFieldClient = (clientProps: TextFieldClientProps & SlugFieldPro
   )
 }
 
-export default SlugFieldClient
-
+// InfoIcon - keeping as custom for now since Payload's Info icon may not be publicly accessible
 const InfoIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
@@ -188,21 +188,4 @@ const InfoIcon = () => (
   </svg>
 )
 
-const RefreshIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
-    <path d="M3 3v5h5" />
-    <path d="M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16" />
-    <path d="M16 16h5v5" />
-  </svg>
-)
+export default SlugFieldClient
